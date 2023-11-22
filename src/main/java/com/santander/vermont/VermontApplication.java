8ad62918d84b83a7b9f13688d0f975e36ad56607
manutesting <manu@git.com>
2023-11-22 13:53:10 +0100

    Fix customOpenAPI

--- src/main/java/com/santander/vermont/VermontApplication.java
+++ src/main/java/com/santander/vermont/VermontApplication.java
@@ -3,12 +3,14 @@ package com.santander.vermont;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.santander.vermont")
 public class VermontApplication {
 
 	public static void main(String[] args) {
